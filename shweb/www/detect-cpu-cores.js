8b0d47ea68987b8b4d6ae51be33dc3ac2821a8e5
tuhana <tuhana.cat+git@gmail.com>
2025-09-27 17:58:51 +0300

    Add a script to detect core amount

---
+++ shweb/www/detect-cpu-cores.js
@@ -0,0 +1,5 @@
+const amountOfThreads = navigator.hardwareConcurrency;
+if (amountOfThreads) {
+  document.querySelector("span.cpu-cores-count").textContent = amountOfThreads;
+  document.querySelector("input[name='workers-count']").value = amountOfThreads;
+}
