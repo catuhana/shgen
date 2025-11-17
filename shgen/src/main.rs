f81521a0d3ec45b047a9e3d90efa3a2aeb8a1226
tuhana <tuhana.cat+git@gmail.com>
2025-11-17 12:31:29 +0300

    Remove status reporting, cleanups

--- shgen/src/main.rs
+++ shgen/src/main.rs
@@ -1,13 +1,9 @@
 #![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_possible_truncation)]
 
 use std::{
-    io::Write,
     path::PathBuf,
-    sync::{
-        OnceLock,
-        atomic::{AtomicBool, AtomicU64, Ordering},
-    },
-    time::{Duration, Instant},
+    sync::atomic::{AtomicBool, Ordering},
 };
 
 use rand::RngCore as _;
@@ -23,9 +19,6 @@ use shgen_key_utils::{matcher::Matcher, openssh};
 #[global_allocator]
 static ALLOCATOR: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-static FOUND_KEY: OnceLock<(OpenSSHPublicKey, OpenSSHPrivateKey)> = OnceLock::new();
-
-static KEYS_COUNTER: AtomicU64 = AtomicU64::new(0);
 static STOP_WORKERS: AtomicBool = AtomicBool::new(false);
 
 fn main() {
@@ -57,52 +50,11 @@ fn main() {
                         });
                     }
 
-                    worker(&matcher);
+                    worker(&matcher)
                 })
                 .expect("failed to spawn worker thread"),
         );
     }
-
-    let status_thread = std::thread::Builder::new()
-    .name("status-report".to_string())
-    .spawn(|| {
-        let start_time = Instant::now();
-
-        let mut last_keys_count = 0u64;
-        let mut last_check_time = Instant::now();
-
-        while !STOP_WORKERS.load(Ordering::Relaxed) {
-            // TODO: Maybe make this configurable
-            std::thread::sleep(Duration::from_secs(2));
-
-            let now = Instant::now();
-            let elapsed_total = start_time.elapsed();
-            let elapsed_since_last = now.duration_since(last_check_time);
-
-            let generated_keys = KEYS_COUNTER.load(Ordering::Relaxed);
-
-            let keys_since_last = generated_keys - last_keys_count;
-            let instant_rate = keys_since_last as f64 / elapsed_since_last.as_secs_f64();
-
-            let overall_rate = generated_keys as f64 / elapsed_total.as_secs_f64();
-
-            let formatted_elapsed = format!(
-                "{:02}:{:02}:{:02}",
-                elapsed_total.as_secs() / 3600,
-                (elapsed_total.as_secs() % 3600) / 60,
-                elapsed_total.as_secs() % 60
-            );
-
-            print!(
-                "\r\x1b[K[{formatted_elapsed}] {generated_keys} keys @ {overall_rate:.0} keys/s avg. @ {instant_rate:.0} keys/s inst."
-            );
-            let _ = std::io::stdout().flush();
-
-            last_keys_count = generated_keys;
-            last_check_time = now;
-        }
-    })
-    .expect("failed to spawn status thread");
 
     if let Some(ref mut keep_awake) = keep_awake
         && let Err(error) = keep_awake.prevent_sleep()
@@ -112,19 +64,22 @@ fn main() {
 
     println!("{}", config.generate_config_overview());
 
-    status_thread.join().unwrap();
+    let mut found_key: Option<(OpenSSHPublicKey, OpenSSHPrivateKey)> = None;
     for handle in worker_handles {
-        handle.join().unwrap();
+        if let Ok(Some(key_pair)) = handle.join()
+            && found_key.is_none()
+        {
+            found_key = Some(key_pair);
+            STOP_WORKERS.store(true, Ordering::Release);
+        }
     }
 
-    if let Some((public_key, private_key)) = FOUND_KEY.get() {
-        // TODO: Print stuff like which keywords matched, in which fields,
-        // how long it took, which thread found it, etc.
-        config.output.save_keys(public_key, private_key);
+    if let Some((public_key, private_key)) = found_key {
+        config.output.save_keys(&public_key, &private_key);
     }
 }
 
-fn worker(matcher: &Matcher) {
+fn worker(matcher: &Matcher) -> Option<(OpenSSHPublicKey, OpenSSHPrivateKey)> {
     const BATCH_COUNT: usize = (8 * 1024) / SECRET_KEY_LENGTH;
 
     let mut rng = Rng::from_best_available();
@@ -142,16 +97,13 @@ fn worker(matcher: &Matcher) {
             if let Some((public_key, private_key)) =
                 matcher.search_matches(&mut formatter, &mut rng)
             {
-                if FOUND_KEY.set((public_key, private_key)).is_ok() {
-                    STOP_WORKERS.store(true, Ordering::Release);
-                }
-
-                return;
+                STOP_WORKERS.store(true, Ordering::Release);
+                return Some((public_key, private_key));
             }
         }
-
-        KEYS_COUNTER.fetch_add(BATCH_COUNT as u64, Ordering::Relaxed);
     }
+
+    None
 }
 
 fn parse_config_path_arg() -> Option<PathBuf> {
